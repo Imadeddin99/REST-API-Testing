0176537ab730e7270b59350125ca2f6cec804387
ImadSalama <ce.emad1999@gmail.com>
2021-01-18 16:04:13 +0200

    Salary range done here

--- src/test/java/GETTest.java
+++ src/test/java/GETTest.java
@@ -7,9 +7,9 @@ import org.testng.annotations.Test;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
 public class GETTest {
@@ -35,29 +35,10 @@ public class GETTest {
         }
     }
 
-    @Test
-    public void test1() throws IOException, JSONException {
-        URL url = new URL("http://192.168.200.91:8080/demo-server/employee-module/Imad/");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setDoOutput(true);
-        int status = con.getResponseCode();
-byte[] result = new byte[2000];
-        InputStream in = con.getInputStream();
-        InputStreamReader isReader = new InputStreamReader(in);
-        BufferedReader reader = new BufferedReader(isReader);
-        StringBuffer sb = new StringBuffer();
-        String str;
-        while((str = reader.readLine())!= null){
-            sb.append(str);
-        }
-        JSONAssert.assertEquals(sb.toString(), originalData, false);
 
-    }
 
-    @Test
-    public void test2() throws IOException, JSONException {
-        URL url = new URL("http://192.168.200.91:8080/demo-server/employee-module/Imad/5000/10000");
+    public String sendGetRequest(String Url) throws IOException {
+        URL url = new URL(Url);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setDoOutput(true);
@@ -71,10 +52,42 @@ byte[] result = new byte[2000];
         while((str = reader.readLine())!= null){
             sb.append(str);
         }
-        JSONAssert.assertEquals(sb.toString(), dataInBoundaries, false);
+
+
+        return sb.toString();
+    }
+    @Test
+    public void test1() throws IOException, JSONException {
+       String response =sendGetRequest("http://192.168.200.91:8080/demo-server/employee-module/Imad/");
+        JSONAssert.assertEquals(response.toString(), originalData, false);
 
     }
 
+    @Test
+    public void TestGETWithNumbersInrangeOfSalaries() throws IOException, JSONException {
+        int num1=5000;
+        int num2=10000;
+       String response=sendGetRequest("http://192.168.200.91:8080/demo-server/employee-module/Imad/"+num1+"/"+num2);
+        JSONAssert.assertEquals(response.toString(), dataInBoundaries, false);
+
+    }
+    @Test
+    public void TestGETWithNumbersOutrangeOfSalariesAbove() throws IOException, JSONException {
+        int num1=90000;
+        int num2=100000;
+        String response=sendGetRequest("http://192.168.200.91:8080/demo-server/employee-module/Imad/"+num1+"/"+num2);
+        org.json.JSONObject json=new org.json.JSONObject(response);
+        assertEquals(json.getInt("employeesCount"),0);
+    }
+
+    @Test
+    public void TestGETWithNumbersOutrangeOfSalariesLess() throws IOException, JSONException {
+        int num1=100;
+        int num2=200;
+        String response=sendGetRequest("http://192.168.200.91:8080/demo-server/employee-module/Imad/"+num1+"/"+num2);
+        org.json.JSONObject json=new org.json.JSONObject(response);
+        assertEquals(json.getInt("employeesCount"),0);
+    }
 
 
 
